058ab274426be379b23e4dcb5c63970a62dd1f1e
Jackson Greer <greer2jl@dukes.jmu.edu>
2023-09-25 16:52:41 -0400

    demo sync changes

--- demo_sync.py
+++ demo_sync.py
@@ -5,77 +5,96 @@ import datasets
 
 PIECE_TITLE = "Air for Band Bass Line"
 PLAYING_SAMPLE_PATH = "/cs/home/stu/greer2jl/Documents/tele.band-playing-samples/"
-PROCESSED_DATASET_PATH = "processed_dataset/teleband_dataset_mp3"
+PROCESSED_DATASET_PATH = "/cs/home/stu/greer2jl/Documents/teleband-export/teleband-export/teleband_dataset_mp3"
 MP3_PATH = "/cs/home/stu/greer2jl/Documents/teleband-export/teleband-export/teleband_wavs"
 
 
 #loading and getting everything set with the sample piece
-p_data, p_sr = librosa.load(f'{PLAYING_SAMPLE_PATH}{PIECE_TITLE}.mp3')
+playing_sample_data, playing_sample_sr = librosa.load(f'{PLAYING_SAMPLE_PATH}{PIECE_TITLE}.mp3')
 
 dataset = datasets.load_from_disk(PROCESSED_DATASET_PATH)
-dataset = dataset.filter(lambda a : a["title"] == PIECE_TITLE and a["rhythm"] == 3)
-
-
+bad_score = dataset.filter(lambda a : a["title"] == PIECE_TITLE and a["rhythm"] == 2)[0]
+good_score = dataset.filter(lambda a : a["title"] == PIECE_TITLE and a["rhythm"] == 5)[0]
+print(good_score)
 
 #loading the actual mp3 file with libroas
-s_data, s_sr = dataset[1]["key"]["array"], dataset[1]["key"]["sampling_rate"]
+bad_data, bad_sr = bad_score["key"]["array"], bad_score["key"]["sampling_rate"]
+good_data, good_sr = good_score["key"]["array"], good_score["key"]["sampling_rate"]
 
 
 
 hop_length = 1024
 
 #This holds the result of the chromagram comp. It is a matrix representing the chromograph over time
-p_chroma = librosa.feature.chroma_cqt(y=p_data, sr=p_sr,
+playing_sample_chroma = librosa.feature.chroma_cqt(y=playing_sample_data, sr=playing_sample_sr,
                                          hop_length=hop_length)
-s_chroma = librosa.feature.chroma_cqt(y=s_data, sr=p_sr,
+bad_chroma = librosa.feature.chroma_cqt(y=bad_data, sr=bad_sr,
+                                         hop_length=hop_length)
+good_chroma = librosa.feature.chroma_cqt(y=good_data, sr=good_sr,
                                          hop_length=hop_length)
 
 
 # --- Dynamic Time Warping --- 
 
-D, wp = librosa.sequence.dtw(X=p_chroma, Y=s_chroma, metric='cosine')
+dynamic_fig, (bad_ax, good_ax) = plt.subplots(nrows=2)
+
+# -----
+
+bad_D, bad_wp = librosa.sequence.dtw(X=playing_sample_chroma, Y=bad_chroma, metric='cosine')
 
 
-wp_s = librosa.frames_to_time(wp, sr=p_sr, hop_length=hop_length)
+bad_wp_s = librosa.frames_to_time(bad_wp, sr=playing_sample_sr, hop_length=hop_length)
 
-fig, ax = plt.subplots()
+bad_img = librosa.display.specshow(bad_D, x_axis='time', y_axis='time', sr=playing_sample_sr,
+                               cmap='gray_r', hop_length=hop_length, ax=bad_ax)
 
-
-img = librosa.display.specshow(D, x_axis='time', y_axis='time', sr=p_sr,
-                               cmap='gray_r', hop_length=hop_length, ax=ax)
-
-ax.plot(wp_s[:, 1], wp_s[:, 0], marker='o', color='r')
-ax.set(title='Warping Path on Acc. Cost Matrix $D$',
+bad_ax.plot(bad_wp_s[:, 1], bad_wp_s[:, 0], marker='o', color='r')
+bad_ax.set(title='Bad Warping Path on Acc. Cost Matrix $D$',
        xlabel='Time $(X_2)$', ylabel='Time $(X_1)$')
-fig.colorbar(img, ax=ax)
+dynamic_fig.colorbar(bad_img, ax=bad_ax)
+
+# -- -
+
+good_D, good_wp = librosa.sequence.dtw(X=playing_sample_chroma, Y=good_chroma, metric='cosine')
+
+
+good_wp_s = librosa.frames_to_time(good_wp, sr=playing_sample_sr, hop_length=hop_length)
+
+good_img = librosa.display.specshow(good_D, x_axis='time', y_axis='time', sr=playing_sample_sr,
+                               cmap='gray_r', hop_length=hop_length, ax=good_ax)
+
+good_ax.plot(good_wp_s[:, 1], good_wp_s[:, 0], marker='o', color='r')
+good_ax.set(title='Good Warping Path on Acc. Cost Matrix $D$',
+       xlabel='Time $(X_2)$', ylabel='Time $(X_1)$')
+dynamic_fig.colorbar(good_img, ax=good_ax)
 
 
 # -- Alternative Visualization -- 
 
-from matplotlib.patches import ConnectionPatch
+# from matplotlib.patches import ConnectionPatch
 
-fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=True, sharey=True, figsize=(8,4))
+# fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=True, sharey=True, figsize=(8,4))
 
-# Plot x_2
-librosa.display.waveshow(p_data, sr=p_sr, ax=ax2)
-ax2.set(title='Sample Piece')
+# # Plot x_2
+# librosa.display.waveshow(playing_sample_data, sr=playing_sample_sr, ax=ax2)
+# ax2.set(title='Sample Piece')
 
-# Plot x_1
-librosa.display.waveshow(s_data, sr=s_sr, ax=ax1)
-ax1.set(title='Student Performance')
-ax1.label_outer()
+# # Plot x_1
+# librosa.display.waveshow(bad_data, sr=bad_sr, ax=ax1)
+# ax1.set(title='Student Performance')
+# ax1.label_outer()
 
 
-n_arrows = 20
-for tp1, tp2 in wp_s[::len(wp_s)//n_arrows]:
-    # Create a connection patch between the aligned time points
-    # in each subplot
-    con = ConnectionPatch(xyA=(tp1, 0), xyB=(tp2, 0),
-                          axesA=ax1, axesB=ax2,
-                          coordsA='data', coordsB='data',
-                          color='r', linestyle='--',
-                          alpha=0.5)
-    con.set_in_layout(False)  # This is needed to preserve layout
-    ax2.add_artist(con)
+# n_arrows = 20
+# for tp1, tp2 in bad_wp_s[::len(bad_wp_s)//n_arrows]:
+#     # Create a connection patch between the aligned time points
+#     # in each subplot
+#     con = ConnectionPatch(xyA=(tp1, 0), xyB=(tp2, 0),
+#                           axesA=ax1, axesB=ax2,
+#                           coordsA='data', coordsB='data',
+#                           color='r', linestyle='--',
+#                           alpha=0.5)
+#     con.set_in_layout(False)  # This is needed to preserve layout
+#     ax2.add_artist(con)
 
 plt.show()
